01713c301b597b32609c83f07f82a42a0790015e
juliodelfino <julius.delfino@yahoo.com>
2019-09-20 23:19:00 +0800

    Added test buses endpoint

---
+++ src/main/java/com/delfino/sgbus/model/Bus.java
@@ -0,0 +1,11 @@
+package com.delfino.sgbus.model;
+
+public class Bus {
+
+    public String no;
+    public String operator;
+    public BusTime next;
+    public BusTime next2;
+    public BusTime next3;
+    public BusTime subsequent;
+}
